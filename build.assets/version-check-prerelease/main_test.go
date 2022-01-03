77437083be76515ea0f85eeac98e6fda015f7f9f
Walt Della <walt@goteleport.com>
2022-01-03 13:31:10 -0800

    squash - improve prerelease tests

--- build.assets/version-check-prerelease/main_test.go
+++ build.assets/version-check-prerelease/main_test.go
@@ -9,37 +9,37 @@ func TestCheck(t *testing.T) {
 		desc     string
 		tag      string
 		releases []string
-		err      bool
+		wantErr  bool
 	}{
 		{
-			desc: "fail-rc",
-			tag:  "v9.0.0-rc.1",
-			err:  true,
+			desc:    "fail-rc",
+			tag:     "v9.0.0-rc.1",
+			wantErr: true,
 		},
 		{ // this build was published to the deb repos on 2021-10-06
-			desc: "fail-debug",
-			tag:  "v6.2.14-debug.4",
-			err:  true,
+			desc:    "fail-debug",
+			tag:     "v6.2.14-debug.4",
+			wantErr: true,
 		},
 		{
-			desc: "fail-metadata",
-			tag:  "v8.0.7+1a2b3c4d",
-			err:  true,
+			desc:    "fail-metadata",
+			tag:     "v8.0.7+1a2b3c4d",
+			wantErr: true,
 		},
 		{
-			desc: "pass",
-			tag:  "v8.0.1",
-			err:  false,
+			desc:    "pass",
+			tag:     "v8.0.1",
+			wantErr: false,
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.desc, func(t *testing.T) {
 			err := check(test.tag)
-			if test.err && err == nil {
-				t.Fatalf("Expected an error, got nil.")
+			if test.wantErr && err == nil {
+				t.Errorf("Expected an error, got nil.")
 			}
-			if !test.err && err != nil {
-				t.Fatalf("Did not expect and error, got: %v", err)
+			if !test.wantErr && err != nil {
+				t.Errorf("Did not expect and error, got: %v", err)
 			}
 		})
 	}
